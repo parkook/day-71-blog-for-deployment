90f50332d91618858ee3867b80f4230eb841fc78
morty <mparkook@yahoo.co.uk>
2024-11-11 20:37:05 +0000

    removed the duplicated contact route.

--- main.py
+++ main.py
@@ -123,7 +123,7 @@ def admin_only(f):
         # Otherwise continue with the route function
         return f(*args, **kwargs)
 
-    decorated_function.__name__ = f.__name__
+    # decorated_function.__name__ = f.__name__
     return decorated_function
 
 
